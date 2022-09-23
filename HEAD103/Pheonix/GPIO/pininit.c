a0a452ade2b73364944fb916f4d2276ec21b8f82
lvana <falcon198236@outlook.com>
2022-09-24 03:01:53 +0800

    1.004 9/23/2022

--- HEAD103/Pheonix/GPIO/pininit.c
+++ HEAD103/Pheonix/GPIO/pininit.c
@@ -17,10 +17,29 @@ void InitGPIO(void)
 	AFIO->MAPR |= 0x02000000; //FOR JTAG
 
 	pinInit(LED_HEARTBEAT);
+	pinInit(LED_CAN_TX);
+	pinInit(LED_CAN_RX);
+#ifdef USE_LCD
+#else
+	pinInit(LED_HEATER_POWER);
+	pinInit(LED_FANCOD_POWER);
+	pinInit(LED_MOTOR_ENABLE);
+	pinInit(LED_MOTOR_POWER);
+#endif
 	pinInit(PIN_CAN_RX);
 	pinInit(PIN_CAN_TX);
 	pinInit(PIN_ADC_POSITION);
 }
 
+void InitLeds()
+{
+	LED_CAN_RX_OFF;
+	LED_CAN_TX_OFF;
+	LED_ERROR_OFF;
+	LED_HEARTBEAT_OFF;
+	LED_MOTOR_ENABLE_OFF;
+	LED_FANCOD_POWER_OFF;
+	LED_MOTOR_POWER_OFF;
+}
 
 #endif /* PININIT_C_ */
